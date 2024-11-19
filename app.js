84ba13f5091e05206abb99c55841458190e3bff6
jonathan <bayart.jonathan@laposte.net>
2024-11-19 15:43:50 +0100

    carousel

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("hello le monde");
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlider: true,
+  initialSlide: 2,
+  speed: 600,
+  loop: true,
+  // autoplay: {
+  //   delay: 1500,
+  // },
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slidesShadows: true,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
